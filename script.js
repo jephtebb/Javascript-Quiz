90d207b798b3b391a2cc11ea07b3208f5207a30c
Jephtebb <jephteborno@yahoo.com>
2020-11-07 14:44:13 -0500

    debugging code

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 var quizScore = 0;
 var questionTracker = 0;
 var listId = 0;
+var counter = 0;
+var correctAnswers = ['Fritay', 'Jeff', 'Ayahna', 'January', '2015', 'Soccer'];
 var questionsAnswers = [
     {
         questions: 'What is my favorite food?',
@@ -21,53 +23,75 @@ var questionsAnswers = [
     },
     {
         questions: 'What year did I gradute with my bachelor',
-        choices: ['2016','2015','2018', '2017'],
+        choices: ['2016', '2015', '2018', '2017'],
     },
     {
         questions: 'What sport do I watch the most?',
-        choices: ['Football', 'Basketball','Soccer', 'Baseball'],
+        choices: ['Football', 'Basketball', 'Soccer', 'Baseball'],
     }
 ]
 // var choicesList = document.createElement("ol");
 //  var questionsHeader = document.createElement("h1");
 // var multipleChoice = document.querySelector("#multipleChoice")
-function preparedQuestion(){
+function preparedQuestion() {
     var choicesList = document.createElement("ol");
     var questionsHeader = document.createElement("h1");
     var multipleChoice = document.querySelector("#multipleChoice");
     var quests = questionsAnswers[questionTracker].questions;
     questionsHeader.textContent = quests;
     multipleChoice.appendChild(questionsHeader);
-    for (var i =0; i<Object.keys(questionsAnswers[questionTracker].choices).length; i++){
+    for (var i = 0; i < Object.keys(questionsAnswers[questionTracker].choices).length; i++) {
         var listItem = document.createElement("li");
+        listItem.className = "value";
         choicesList.appendChild(listItem);
         listItem.textContent = questionsAnswers[questionTracker].choices[i];
-        listItem.setAttribute("data-list-id", listId);
+        listItem.setAttribute("data-list-id", listItem.textContent);
+        listItem.setAttribute("id", "value" + counter);
+        //console.log(listItem);
         listItem.style.border = "1px solid #0000FF";
         listItem.style.padding = "10px 30px 10px 30px";
         listItem.style.width = "120px";
         listItem.style.backgroundColor = "lightslategray";
         listItem.style.marginBottom = "5px";
         multipleChoice.appendChild(listItem);
-        listId++;
-   }
+        var targetId = "value" + counter;
+        var choiceId = document.getElementById(targetId);
+        //console.log(choiceId);
+        choiceId.addEventListener("click", function () {
+            var uniqueId = choiceId.getAttribute("data-list-id");
+            console.log(choiceId);
+            if (correctAnswers.includes(uniqueId)){
+                alert("Correct!");
+                quizScore++;
+            }
+            else {
+                alert("Wrong!");
+            }
+            questionTracker++;
+            preparedQuestion();
+        });
+        console.log("called");
+       counter++;
+       
+    }
+
 }
 var askQuestion = document.querySelector("#start");
 askQuestion.addEventListener("click", preparedQuestion);
 
 
-var userChoice = document.querySelector("#multipleChoice");
-userChoice.addEventListener("click", function(listId){
-    var uniqueId = userChoice.getAttribute("data-list-id");
-    if (uniqueId === listId){
-        alert("Correct!");
-        quizScore++;
-    }
-    else{
-        alert("Wrong!");
-    }
-    questionTracker++;
-   // userChoice.remove();
-    preparedQuestion();
-});
+// var userChoice = document.querySelector("#multipleChoice");
+// userChoice.addEventListener("click", function (listId) {
+//     var uniqueId = userChoice.getAttribute("data-list-id");
+//     console.log(uniqueId);
+//     if (correctAnswers.includes(uniqueId)) {
+//         alert("Correct!");
+//         quizScore++;
+//     }
+//     else {
+//         alert("Wrong!");
+//     }
+//     questionTracker++;
+//     preparedQuestion();
+// });
 
